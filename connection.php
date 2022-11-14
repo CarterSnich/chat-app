9b20ded6e58af43bd3aedd196698db02f4110c53
CarterSnich <jmestreraclosa@gmail.com>
2022-11-14 21:40:39 +0800

    fixed connection database name

--- connection.php
+++ connection.php
@@ -1,11 +1,12 @@
 <?php
 $servername = "remotemysql.com";
+$dbname = "kjQ1QLBFTp";
 $username = "kjQ1QLBFTp";
 $password = "A4Podp4JmM";
 $port = 3306;
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=chat_app;port=$port", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "Connected successfully";
